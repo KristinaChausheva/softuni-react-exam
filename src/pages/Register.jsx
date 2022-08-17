a3999d3501fed8ef39c10b27e3e3a14ec13fda57
Kristina Chausheva <k.chausheva@gmail.com>
2022-08-17 17:23:01 +0300

    adding basic register form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,66 @@
-import React from "react"
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
 
 function Register() {
-  return <div>Register</div>
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    name: "",
+  })
+  const { email, password, name } = formData
+
+  const navigate = useNavigate()
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }))
+  }
+  return (
+    <>
+      <div className="container">
+        <header>
+          <p>Register!</p>
+        </header>
+        <main>
+          <form>
+            <input
+              type="text"
+              className="nameInput"
+              placeholder="Name"
+              id="name"
+              value={name}
+              onChange={onChange}
+            />
+            <input
+              type="email"
+              className="emailInput"
+              placeholder="Email"
+              id="email"
+              value={email}
+              onChange={onChange}
+            />
+
+            <input
+              type="password"
+              className="passwordInput"
+              placeholder="Password"
+              id="password"
+              value={password}
+              onChange={onChange}
+            />
+
+            <Link to="forgot-pass">Fotgot Password</Link>
+
+            <button>Register</button>
+          </form>
+
+          <Link to="/login">Login</Link>
+        </main>
+      </div>
+    </>
+  )
 }
 
 export default Register
