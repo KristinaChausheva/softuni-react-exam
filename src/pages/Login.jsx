43024337166153d29b9e631a53b779569199e9df
Kristina Chausheva <k.chausheva@gmail.com>
2022-08-17 17:17:22 +0300

    adding basic login functionalities

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,58 @@
-import React from "react"
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
 
 function Login() {
-  return <div>Login</div>
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  })
+  const { email, password } = formData
+
+  const navigate = useNavigate()
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }))
+  }
+
+  return (
+    <>
+      <div className="container">
+        <header>
+          <p>Welcome Back!</p>
+        </header>
+        <main>
+          <form>
+            <input
+              type="email"
+              className="emailInput"
+              placeholder="Email"
+              id="email"
+              value={email}
+              onChange={onChange}
+            />
+
+            <input
+              type="password"
+              className="passwordInput"
+              placeholder="Password"
+              id="password"
+              value={password}
+              onChange={onChange}
+            />
+
+            <Link to="forgot-pass">Fotgot Password</Link>
+
+            <button>Login</button>
+          </form>
+
+          <Link to="/register">Register</Link>
+        </main>
+      </div>
+    </>
+  )
 }
 
 export default Login
