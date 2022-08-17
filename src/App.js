88085297f73e218d33727e1696dcbee762178f77
Kristina Chausheva <k.chausheva@gmail.com>
2022-08-17 16:18:01 +0300

    adding router and some basic pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Profile from "./pages/Profile"
+import ForgotPass from "./pages/ForgotPass"
+
 function App() {
-  return <div>ELS</div>
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/forgot-pass" element={<ForgotPass />} />
+        </Routes>
+      </Router>
+    </>
+  )
 }
 
 export default App
