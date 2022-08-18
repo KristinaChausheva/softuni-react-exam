7d13fc23b97fb46eb232ee4fbb35ebae6b909a4b
Kristina Chausheva <k.chausheva@gmail.com>
2022-08-18 04:54:52 +0300

    team page styling

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,6 +1,8 @@
 import styles from "./Team.module.css"
 
 import person1 from "../assets/person1.png"
+import person2 from "../assets/person2.png"
+import person3 from "../assets/person3.png"
 
 function Team() {
   return (
@@ -25,7 +27,7 @@ function Team() {
       <div className={styles.instructor}>
         <div className={styles.text}>
           <h2 className={styles.h2}>
-            Hi, I am John Doe, your English teacher.
+            Hi, I am Jane Doe, your English teacher.
           </h2>
           <div className={styles.desc}>
             2007-2010 DaF-Studium Uni Greifswald, over 10 yeras of experience as
@@ -33,14 +35,14 @@ function Team() {
           </div>
         </div>
         <div className={styles.person}>
-          <img src={person1} alt="" className={styles.img} />
+          <img src={person2} alt="" className={styles.img} />
         </div>
       </div>
 
       <div className={styles.instructor}>
         <div className={styles.text}>
           <h2 className={styles.h2}>
-            Hi, I am John Doe, your English teacher.
+            Hi, I am Joan Doe, your English teacher.
           </h2>
           <div className={styles.desc}>
             2007-2010 DaF-Studium Uni Greifswald, over 10 yeras of experience as
@@ -48,7 +50,7 @@ function Team() {
           </div>
         </div>
         <div className={styles.person}>
-          <img src={person1} alt="" className={styles.img} />
+          <img src={person3} alt="" className={styles.img} />
         </div>
       </div>
     </div>
