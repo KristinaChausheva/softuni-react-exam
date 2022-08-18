76570b31418113dc544173fa3eab66edbd3fdae7
Kristina Chausheva <k.chausheva@gmail.com>
2022-08-18 18:39:54 +0300

    contact page implementation

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,5 @@
 import styles from "./Contact.module.css"
+import envelope from "../assets/envelope.svg"
 
 function Contact() {
   return (
@@ -10,16 +11,35 @@ function Contact() {
             If you have questions, you can write me here
           </div>
           <div className={styles.emailInfo}>
-            <img src="" alt="" />
+            <img src={envelope} alt="" width={30} />
             <div>englishteacher@gmail.com</div>
           </div>
         </div>
-
-        <form action="" className={styles.form}>
-          <input type="text" id="name" />
-          <input type="email" id="email" />
-          <textarea name="" id="" cols="30" rows="10"></textarea>
-        </form>
+        <div className={styles.formContainer}>
+          <h3>White a message</h3>
+          <form action="" className={styles.form}>
+            <input
+              type="text"
+              id="name"
+              className={styles.input}
+              placeholder="Name"
+            />
+            <input
+              type="email"
+              id="email"
+              className={styles.input}
+              placeholder="Email"
+            />
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="10"
+              placeholder="Type your message here..."
+            ></textarea>
+            <button className={styles.sendBtn}>Send</button>
+          </form>
+        </div>
       </div>
     </div>
   )
