2e45e671b258b5a2aa343dfabf1aac9ede46d9d1
Kristina Chausheva <k.chausheva@gmail.com>
2022-08-19 17:29:09 +0300

    handling auth state with custom hook

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,8 +1,13 @@
 import { Navigate, Outlet } from "react-router-dom"
+import { useAuthStatus } from "../hooks/useAuthStatus"
 
 function PrivateRoute() {
-  const isLogged = true
-  return isLogged ? <Outlet /> : <Navigate to="/login" />
+  const { loggedIn, checkingStatus } = useAuthStatus()
+
+  if (checkingStatus) {
+    return <h3>Loading ...</h3>
+  }
+  return loggedIn ? <Outlet /> : <Navigate to="/login" />
 }
 
 export default PrivateRoute
